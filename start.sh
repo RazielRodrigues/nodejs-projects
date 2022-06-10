8e039888a0312b9b24d6768ea1fb1ab480e865d2
Raziel Rodrigues <razielx3@live.com>
2022-06-10 16:07:43 -0300

    final docker impletation

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker build -t express-app .
-docker run -p 3002:3002 express-app
-
+cd app
+exec docker build -t express-app .
+exec docker run -p 3002:3002 express-app
 echo "access: http://localhost:3002"
