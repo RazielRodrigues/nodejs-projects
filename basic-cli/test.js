08ecd639cf20c207c1f2bc71b04c5ed55eb5ebaf
Raziel Rodrigues <raziel.rodrigues@pjbank.com.br>
2022-06-24 23:08:52 -0300

    full tests coverage

--- basic-cli/test.js
+++ basic-cli/test.js
@@ -1,14 +1,21 @@
 const { deepEqual } = require('assert');
 const database = require('./database');
 const DEFAULT_ITEM_CADASTRAR = {
-    nome: 'flash',
-    poder: 'speed',
-    id: 1
+    "nome": "flash",
+    "poder": "speed",
+    "id": 1
+}
+
+const DEFAULT_ITEM_ATUALIZAR = {
+    "nome": "Lanterna verde",
+    "poder": "energia do anel",
+    "id": 2
 }
 
 describe('Suite de manipulacao de herois', () => {
 
     before( async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
     })
 
@@ -23,6 +30,30 @@ describe('Suite de manipulacao de herois', () => {
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         const [ actual ] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
         deepEqual(actual, expected);
+    });
+
+    it("deve remover heroi por id", async () => {
+        const expected = true;
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected);
+    });
+
+    it("deve atualizar heroi por id", async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        };
+
+        const novoDado = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+
+        deepEqual(resultado, expected);
     });
 
 });
